a4b48b6a453b8d9d55629001e3ba53cfff9f1556
Nicolas Pellerin <nickypellerin@gmail.com>
2021-02-20 11:26:35 -0500

    Adds ogimage + favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App: any) => (props: any) =>
             sheet.collectStyles(<App {...props} />),
-        });
+        })
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
@@ -22,9 +22,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      };
+      }
     } finally {
-      sheet.seal();
+      sheet.seal()
     }
   }
 
@@ -34,25 +34,21 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <meta
-            name="og:title"
-            content="LearnFramerMotion.com - From beginner to advanced"
-          />
-          <meta name="og:url" content="https://learnframermotion.com" />
+          <meta name="og:title" content="Coding With Nico" />
+          <meta name="og:url" content="https://codingwithnico.com" />
           <meta
             name="description"
-            content="Interactive video course that teaches you all things animation using the powerful React library Framer Motion."
+            content="Learn new life changing coding skills"
           />
           <meta
             name="og:description"
-            content="Interactive video course that teaches you all things animation using the powerful React library Framer Motion.
-            "
+            content="Learn new life changing coding skills"
           />
-          <meta name="og:image" content="/images/og-image.jpg" />
-          <meta name="theme-color" content="#603cba" />
+          <meta name="og:image" content="/og-image.jpg" />
+          <meta name="theme-color" content="#3D28C3" />
           <link rel="icon" type="image/png" href="/favicon.png" />
-          <meta name="theme-color" content="#603cba" />
-          <link
+          <meta name="theme-color" content="#3D28C3" />
+          {/* <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/icons/apple-touch-icon.png"
@@ -61,8 +57,8 @@ export default class MyDocument extends Document {
             rel="mask-icon"
             href="/icons/safari-pinned-tab.svg"
             color="#603cba"
-          />
-          <meta name="msapplication-TileColor" content="#603cba" />
+          /> */}
+          <meta name="msapplication-TileColor" content="#3D28C3" />
           {/* <link rel="manifest" href="/manifest.json" /> */}
           {/* <script
             dangerouslySetInnerHTML={{
@@ -94,6 +90,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
