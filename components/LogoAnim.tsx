e580b4f2800b87ecf04cccef6c68bec5db9b35a4
Nicolas Pellerin <nickypellerin@gmail.com>
2021-03-19 16:54:25 -0400

    Memoize logo component

--- components/LogoAnim.tsx
+++ components/LogoAnim.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, memo } from 'react'
 import styled from 'styled-components'
 import { motion, useAnimation } from 'framer-motion'
 import { useRecoilValue } from 'recoil'
@@ -44,7 +44,7 @@ const LogoAnim = () => {
   )
 }
 
-export default LogoAnim
+export default memo(LogoAnim)
 
 // Styles
 const Logo = styled(motion.img)`
