25ea73a085e402dac912d3231d63013b404118a5
Nicolas Pellerin <nickypellerin@gmail.com>
2021-04-04 16:30:06 -0400

    Remove loading terminal tips and tricks

--- components/SelectTech.tsx
+++ components/SelectTech.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 
 const techImages = [
@@ -12,74 +12,9 @@ const techImages = [
   { tech: 'Rust', logo: '/images/tech/rustlang.png' },
 ]
 
-const list = {
-  visible: {
-    opacity: 1,
-    transition: {
-      when: 'beforeChildren',
-      staggerChildren: 0.2,
-      type: 'spring',
-      damping: 18,
-    },
-  },
-  hidden: {
-    opacity: 0,
-    transition: {
-      when: 'afterChildren',
-    },
-  },
-}
-
-const item = {
-  visible: { opacity: 1, y: 0 },
-  hidden: { opacity: 0, y: 30 },
-}
-
 const SelectTech = () => {
   const [index, setIndex] = useState<number | null>(null)
-  const [perc, setPerc] = useState(0)
-  const [showTech, setShowTech] = useState(false)
   const [minimizeTerminal, setMinimizeTerminal] = useState(false)
-
-  // Saves seenLoadingTech to localStorage
-  let hasSeen: boolean
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return
-    }
-    const storage = localStorage.getItem('seenLoadingTech')
-    hasSeen = storage ? !!JSON.parse(storage) : false
-    if (!hasSeen) {
-      localStorage.setItem('seenLoadingTech', JSON.stringify(true))
-    }
-  }, [])
-
-  //   If hasSeen is true, return early. Else, start loading db count to 100%
-  useEffect(() => {
-    if (hasSeen) {
-      setShowTech(true)
-      return
-    }
-    const inter = window.setInterval(
-      () => setPerc((prevState) => (prevState <= 99 ? prevState + 1 : 100)),
-      10
-    )
-    const idx = window.setTimeout(() => {
-      setShowTech(true)
-      window.clearInterval(inter)
-    }, 1500)
-
-    return () => {
-      window.clearTimeout(idx)
-      window.clearInterval(inter)
-    }
-  }, [])
-
-  //   let laughSound: HTMLAudioElement
-  //   if (typeof Audio !== 'undefined') {
-  //     laughSound = new Audio('/sounds/wiz.mp3')
-  //     laughSound.volume = 0.3
-  //   }
 
   return (
     <Wrapper>
@@ -116,85 +51,54 @@ const SelectTech = () => {
           >
             X
           </button>
-          <AnimatePresence>
-            {showTech ? (
-              <>
-                <Title
-                  initial={{ opacity: 0, y: 10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ type: 'spring', damping: 6 }}
+          <>
+            <Title
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ type: 'spring', damping: 6 }}
+            >
+              Pick a technology
+              <motion.span
+                animate={{ opacity: [0, 1] }}
+                transition={{ yoyo: Infinity }}
+                style={{
+                  display: 'inline-block',
+                  fontSize: '3.4rem',
+                  lineHeight: 1,
+                  marginLeft: 4,
+                }}
+              >
+                &#9646;
+              </motion.span>
+            </Title>
+            <Grid style={{ border: '2px solid rgba(144, 238, 144, 0.1)' }}>
+              {techImages.map(({ tech, logo }, i) => (
+                <Tech
+                  key={tech}
+                  onMouseOver={() => setIndex(i)}
+                  onMouseOut={() => setIndex(null)}
                 >
-                  Pick a technology
-                  <motion.span
-                    animate={{ opacity: [0, 1] }}
-                    transition={{ yoyo: Infinity }}
-                    style={{
-                      display: 'inline-block',
-                      fontSize: '3.4rem',
-                      lineHeight: 1,
-                      marginLeft: 4,
-                    }}
-                  >
-                    &#9646;
-                  </motion.span>
-                </Title>
-                <Grid
-                  variants={list}
-                  initial="hidden"
-                  animate="visible"
-                  style={{ border: '2px solid rgba(144, 238, 144, 0.1)' }}
-                >
-                  <AnimatePresence>
-                    {techImages.map(({ tech, logo }, i) => (
-                      <Tech
-                        key={tech}
-                        onMouseOver={() => setIndex(i)}
-                        onMouseOut={() => setIndex(null)}
-                        variants={item}
-                      >
-                        <Link href={`/tips-tricks/${tech.toLowerCase()}`}>
-                          <a>
-                            <TechImage
-                              layoutId={tech.toLowerCase()}
-                              src={logo}
-                              alt={tech}
-                              transition={{ type: 'tween', duration: 0.2 }}
-                            />
-                            {index === i && (
-                              <Name
-                                initial={{ x: '-50%', opacity: 0, y: 20 }}
-                                animate={{ opacity: 1, y: 0 }}
-                              >
-                                {tech}
-                              </Name>
-                            )}
-                          </a>
-                        </Link>
-                      </Tech>
-                    ))}
-                  </AnimatePresence>
-                </Grid>
-              </>
-            ) : (
-              <LoadingWrapper>
-                <LoadingText initial={{ y: 10 }} animate={{ y: 0 }}>
-                  Loading from database... {perc}%
-                  <motion.span
-                    animate={{ opacity: [0, 1] }}
-                    transition={{ yoyo: Infinity }}
-                    style={{
-                      display: 'inline-block',
-                      fontSize: '3.5rem',
-                      lineHeight: 0.7,
-                      marginLeft: 2,
-                    }}
-                  >
-                    &#9646;
-                  </motion.span>
-                </LoadingText>
-              </LoadingWrapper>
-            )}
-          </AnimatePresence>
+                  <Link href={`/tips-tricks/${tech.toLowerCase()}`}>
+                    <a>
+                      <TechImage
+                        layoutId={tech.toLowerCase()}
+                        src={logo}
+                        alt={tech}
+                      />
+                      {index === i && (
+                        <Name
+                          initial={{ x: '-50%', opacity: 0, y: 20 }}
+                          animate={{ opacity: 1, y: 0 }}
+                        >
+                          {tech}
+                        </Name>
+                      )}
+                    </a>
+                  </Link>
+                </Tech>
+              ))}
+            </Grid>
+          </>
         </Terminal>
         <PhpJoke
           src="/images/php.jpg"
@@ -290,26 +194,6 @@ const Name = styled(motion.span)`
 
   @media (max-width: 500px) {
     display: none;
-  }
-`
-
-const LoadingWrapper = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  z-index: 3;
-  height: 34rem;
-`
-
-const LoadingText = styled(motion.pre)`
-  color: lightgreen;
-  display: flex;
-  font-size: 3rem;
-
-  @media (max-width: 500px) {
-    font-size: 2rem;
   }
 `
 
