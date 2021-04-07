7cf45d7de9f025b61dc7c24a8b94d95054cc5b60
Nicolas Pellerin <nickypellerin@gmail.com>
2021-04-07 17:59:14 -0400

    Fix share component

--- components/Share/Share.tsx
+++ components/Share/Share.tsx
@@ -15,11 +15,7 @@ interface Props {
 
 const Share: FC<Props> = ({ url }) => {
   return (
-    <Wrapper
-      initial={{ x: -200, y: '-50%' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', damping: 16 }}
-    >
+    <Wrapper>
       <ShareText>Share</ShareText>
       <Container>
         <motion.span
@@ -47,47 +43,23 @@ export default Share
 
 // Styles
 const Wrapper = styled(motion.div)`
-  position: absolute;
-  right: 4rem;
-  top: 6rem;
   z-index: 9000;
-
-  @media (min-width: 1200px) {
-    right: unset;
-    left: 0rem;
-    top: 50%;
-    transform: translateY(-50%);
-    background: rgba(255, 255, 255, 0.1);
-    padding: 1.8rem 1.6rem;
-    border-top-right-radius: 0.5rem;
-    border-bottom-right-radius: 0.5rem;
-    box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.1);
-    border: 1px solid #ccc;
-    border-left: none;
-  }
+  padding: 1.8rem 1.6rem;
+  margin-top: 40px;
 `
 
 const Container = styled.div`
   display: flex;
   align-items: center;
-
-  @media (min-width: 1200px) {
-    flex-direction: column;
-    align-items: center;
-  }
+  justify-content: center;
 `
 
 const Facebook = styled(FacebookIcon)`
   border-radius: 50%;
   width: 4.5rem;
   height: 4.5rem;
-  margin-right: 1.8rem;
-
-  @media (min-width: 1200px) {
-    margin-right: 0;
-    margin-bottom: 1.2rem;
-    box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.15);
-  }
+  margin-right: 30px;
+  box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.15);
 `
 
 const Twitter = styled(TwitterIcon)`
@@ -105,37 +77,8 @@ const ShareText = styled.span`
   text-align: center;
   font-size: 1.2rem;
   font-weight: 600;
-  color: rebeccapurple;
-  margin-bottom: 1.1rem;
+  color: var(--primaryColor);
+  margin-bottom: 14px;
   position: relative;
   letter-spacing: 1px;
-
-  &:after {
-    content: '';
-    width: 1px;
-    height: 6px;
-    background: rebeccapurple;
-    position: absolute;
-    left: 27px;
-    bottom: -7px;
-    transform: rotate(45deg);
-  }
-
-  &:before {
-    content: '';
-    width: 1px;
-    height: 6px;
-    background: rebeccapurple;
-    position: absolute;
-    right: 27px;
-    bottom: -7px;
-    transform: rotate(-45deg);
-  }
-
-  @media (min-width: 1200px) {
-    &:after,
-    &:before {
-      display: none;
-    }
-  }
 `
