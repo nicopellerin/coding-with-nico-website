9b07163c1850748a629d48f7981c5e1b76999b9e
Nicolas Pellerin <nickypellerin@gmail.com>
2021-04-09 19:19:16 -0400

    Implements dynamic pages for tips tricks

--- data/graphql.ts
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-        date: 'February 20, 2021',
-        slug: 'graphql/basic-server-apollo',
-        title: 'Basic server with Apollo',
-        tech: 'graphql',
-        image: '/images/card1.png'
-    }
-]
--- data/typescript.ts
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-        date: 'February 20, 2021',
-        slug: 'typescript/basic-react-component-typescript',
-        title: 'Basic React component with Typescript',
-        tech: 'typescript',
-        image: '/images/card1.png'
-    }
-]
